1040a634e21ba77cd616593e2fdd71e2d47c988b
PerthGoat <administrator@ryancdeyong.us>
2021-06-09 19:31:44 -0400

    this seems to be working a lot better now with the new parsing stuff

--- tcl.py
+++ tcl.py
@@ -1,9 +1,9 @@
 import tclparse
 import tclstate
 
-p = tclparse.TCLParse('''set x 10
-set y "40 $x"
-puts $y''')
+p = tclparse.TCLParse('''proc test {x {y 10}} {
+  puts $y
+}''')
 
 parsed = p.PROGRAM()
 
@@ -36,15 +36,69 @@ def F_PUTS(cmd, state):
   assert cmd[0]['WORD'] == 'puts'
   print(cmd[1]['WORD'])
 
+# ARGS = WORD, { WHITESPACE, ('{', WORD, WORD, '}') | WORD } ;
+def parseArguments(args):
+  split_args = args.split(' ')
+  parsed_args = []
+  for i, s in enumerate(split_args):
+    if '{' in s:
+      left = s;
+      right = split_args[i + 1]
+      left = left[1:]
+      right = right[:-1]
+      parsed_args.append({'VAR': left, 'DEFAULT': right})
+    elif '}' not in s:
+      parsed_args.append(s)
+  return parsed_args
+
+def runFuncByName(name, state, *inargs):
+  funcStuff = state.getProc(name)
+  args = funcStuff['args']
+  pargs = parseArguments(args)
+  body = funcStuff['body']
+  newstate = tclstate.TCLState()
+  p2 = tclparse.TCLParse(body)
+  parsed2 = p2.PROGRAM()
+  
+  min_len = len([z for z in pargs if 'DEFAULT' not in z])
+  assert len(inargs) >= min_len
+  assert len(inargs) <= len(pargs)
+  
+  for i, z in enumerate(pargs):
+    if type(z) is dict and i >= len(inargs):
+      newstate.setVar(z['VAR'], z['DEFAULT'])
+    elif type(z) is dict:
+      newstate.setVar(z['VAR'], inargs[i])
+    else:
+      newstate.setVar(z, inargs[i])
+  
+  for c in parsed2:
+    runCmd(c, newstate)
+  
+  #print(parsed2)
+
+def F_PROC(cmd, state):
+  assert cmd[0]['WORD'] == 'proc'
+  
+  fname = cmd[1]['WORD']
+  fargs = cmd[2]['WORD']
+  fbody = cmd[3]['WORD']
+  
+  state.setProc(fname, {'args': fargs, 'body': fbody})
+  
+  runFuncByName('test', state, 14, 6)
+
 def runCmd(cmd, state):
   subcmd(cmd, state)
   
-  print(cmd)
+  #print(cmd)
 
   if cmd[0]['WORD'] == 'set':
     F_SET(cmd, state)
   elif cmd[0]['WORD'] == 'puts':
     F_PUTS(cmd, state)
+  elif cmd[0]['WORD'] == 'proc':
+    F_PROC(cmd, state)
   else:
     print(f"unknown command {cmd}")
   #print(cmd)
